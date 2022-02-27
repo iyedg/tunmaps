6119a5c3eea5307e24237a9f25903dbe8b20932c
Iyed Ghedamsi <iyed.ghedamsi@gmail.com>
2022-02-27 23:20:05 +0100

    Clean column names for governorates and update data source

--- data-raw/tun_governorates.R
+++ data-raw/tun_governorates.R
@@ -1,14 +1,21 @@
-tun_governorates_path <- fs::path("data-raw", "tun_governorates.geojson")
+library(tidyverse)
+source("data-raw/utils.R")
+
+tun_governorates_path <- fs::path("data-raw", "raw", "tun_governorates.geojson")
 
 if (!fs::file_exists(tun_governorates_path)) {
-  governorates_geojson_url <- "http://www.openculture.gov.tn/dataset/70fd9b49-3925-477f-ae17-9d206d650aab/resource/4b3d85dd-a523-425c-b477-ab9fec6e51a0/download/decoupage.geojson"
+  governorates_geojson_url <- "https://www.data4tunisia.org/fr/datasets/r/f2324e88-a273-465a-bd94-c952f2829995"
   download.file(governorates_geojson_url, tun_governorates_path)
 }
 
-tun_governorates <- dplyr::rename(
-  sf::read_sf(tun_governorates_path),
-  id_gov = gov_id,
-  name_gov_fr = gov_name_f
-)
+tun_governorates <- read_sf(tun_governorates_path) %>%
+  rename(
+    id_gov = CIRC_ID,
+    name_fr_gov = NAME_EN,
+    name_ar_gov = NAME_AR,
+  ) %>%
+  mutate(
+    id_gov = as.numeric(str_remove(id_gov, "\\d$"))
+  )
 
 usethis::use_data(tun_governorates, overwrite = TRUE, compress = "xz")
